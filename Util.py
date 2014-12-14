63a5c707293b4a5298bab1741bba0cad0b297877
Benjamin Goldberg <bgoldberg@mallow.cs.hmc.edu>
2014-12-13 16:01:32 -0800

    fixed Message repr

--- Util.py
+++ Util.py
@@ -6,26 +6,26 @@
 import json
 
 class Message:
-	"""
-	Wrapper for JSON message passing between Clients and Servers, and 
-	between Servers
-	"""
+    """
+    Wrapper for JSON message passing between Clients and Servers, and 
+    between Servers
+    """
 
-	def __init__(self, msg_type, action, payload, timestamp, source):
-		self.msg_type = msg_type
-		self.action = action
-		self.payload = payload
-		self.timestamp = timestamp
-		self.source = source
+    def __init__(self, msg_type, action, payload, timestamp, source):
+        self.msg_type = msg_type
+        self.action = action
+        self.payload = payload
+        self.timestamp = timestamp
+        self.source = source
 
-	# Package the message as a JSON string
-	def make_json(self):
-		json_dict = {"msg_type": self.msg_type, 
-		             "action": self.action, 
-		             "payload": self.payload,
-		             "timestamp": self.timestamp,
-		             "source": self.source}
-		return json.dumps(json_dict)
+    # Package the message as a JSON string
+    def make_json(self):
+        json_dict = {"msg_type": self.msg_type, 
+                     "action": self.action, 
+                     "payload": self.payload,
+                     "timestamp": self.timestamp,
+                     "source": self.source}
+        return json.dumps(json_dict)
 
     # Rich comparison operators
     def __eq__(self, other):
@@ -39,11 +39,14 @@ class Message:
     def __le__(self, other):
         return not other.timestamp<self.timestamp
 
-	def __repr__(self):
-		return "msg_type: " + self.msg_type + 
-				"\naction: " + self.action +
-				"\npayload: " + self.payload +
-				"\ntimestamp: " + self.timestamp +
-				"\nsource: " + self.source
+    def __key__(self):
+        return self.timestamp
+
+    def __repr__(self):
+        return "[msg_type: " + self.msg_type + \
+                "\naction: " + self.action + \
+                "\npayload: " + self.payload + \
+                "\ntimestamp: " + self.timestamp + \
+                "\nsource: " + self.source + "]\n"
 
 
